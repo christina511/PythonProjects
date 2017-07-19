50f7690c7dc9ab1cb86ff7799224ebefd7c79a60
christina511 <christina511ny@gmail.com>
2017-07-19 12:53:22 -0700

    Update DrawingShapes.py

--- DrawingShapes.py
+++ DrawingShapes.py
@@ -10,17 +10,36 @@ x_pos = -250
 y_pos = -150
 
 ### Write your code below:
-While True
-    pendown()
-    forward(50)
-    left(120)
-    penup()
+def makeTriangle():
+    for i in range(3):
+        pendown()
+        forward(50)
+        left(120)
+        penup()
 
-While True
-    pendown()
-    forward(100)
-    left(90)
-    penup()
+def makeSquare():
+    for i in range(4):
+        pendown()
+        forward(100)
+        left(90)
+        penup()
+
+#makeTriangle()
+#makeSquare()
+
+def makeShape():
+    sides = int(input('Enter number of sides:'))
+    length = int(input('Enter length:'))
+    for i in range(sides):
+        pendown()
+        forward(length)
+        angles=360/sides
+        left(angles)
+        penup()
+
+#makeShape()
+while True:
+    makeShape()
 
 # Close window on click.
 exitonclick()
